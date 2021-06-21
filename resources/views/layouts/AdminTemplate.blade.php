ccd1377bbdecdd647f0e7cf94c421c6eba1ee800
andi suartika <pt.andisuartika@gmail.com>
2021-06-21 17:48:56 +0800

    projectuasweb

--- resources/views/layouts/AdminTemplate.blade.php
+++ resources/views/layouts/AdminTemplate.blade.php
@@ -75,8 +75,8 @@
           </ul>
           <ul>
             <li class="relative px-6 py-3">
-            <x-sidebar>
-            </x-sidebar>
+            {{-- <x-sidebar>
+            </x-sidebar> --}}
               <a
                 class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                 href="{{ route('mahasiswa.index')}}"
